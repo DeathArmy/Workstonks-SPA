d7af5e13e0ab495a0e067651bae604dc6fb7ea34
DeathArmy <46004028+DeathArmy@users.noreply.github.com>
2021-06-11 22:37:55 +0200

    Poprawki w wysyłaniu formularza

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -1,17 +1,17 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { configResponse } from '../Helpers/configResponse';
+import { formFields } from '../form/form.component';
 
 @Injectable()
 export class FormService {
-  private urlString: string = "https://localhost:5001//api/CarService/";
+  private urlString: string = "https://localhost:5001/api/CarService/";
   constructor(private http: HttpClient) {  }
 
-  postForm(form: any) : Observable<configResponse>
+  postForm(form: any) : Observable<formFields>
   {
     let tempUrl = this.urlString + 'serviceRequest';
     console.log(JSON.stringify(form));
-    return this.http.post<configResponse>(tempUrl, JSON.stringify(form));
+    return this.http.post<formFields>(tempUrl, form);
   }
 }
